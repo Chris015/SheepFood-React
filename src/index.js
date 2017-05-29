04316b5785bbc543821723a6eff06da6d9c6affa
Christopher <christopher.olsson@hyperisland.se>
2017-05-29 18:39:23 +0200

    can now loop through all recipes

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import "./index.css";
 
 
 /*
-    TODO:
-    1. Förstasidan ska bara visa titel, bild och en liten del av 'description'
-    2. Loopa igenom listan med recept, och skapa recept-klasser
-    2.2 Skicka in information om receptet visa props t.ex <Recipe details = {recipeObjektet}/>
+ TODO:
+ 1. Förstasidan ska bara visa titel, bild och en liten del av 'description'
+ 2. Loopa igenom listan med recept, och skapa recept-klasser
+ 2.2 Skicka in information om receptet visa props t.ex <Recipe details = {recipeObjektet}/>
 
-    Jag har inte tänkt igenom hur man begränsar vad man ska visa på framsidan jämfört när man klickar på ett recept.
-    Vi får kanske skapa två typer av klasser. En som visar bara lite grann (framsidan) och en klass som visar allt (detaljvyn)
+ Jag har inte tänkt igenom hur man begränsar vad man ska visa på framsidan jämfört när man klickar på ett recept.
+ Vi får kanske skapa två typer av klasser. En som visar bara lite grann (framsidan) och en klass som visar allt (detaljvyn)
  */
 
 
@@ -20,7 +20,44 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            recipes: [{}]
+            recipes: [
+                {
+                    title: '',
+                    description: '',
+                    imageUrl: '',
+                    recipeItems: [
+                        {
+                            id: null,
+                            ingredient: {name: ''},
+                            amount: null,
+                            unit: {name: ""}
+                        }
+                    ]
+
+                }
+            ],
+            mockRecipes: [
+                {
+                    title: 'Title',
+                    description: 'Description',
+                    imageUrl: 'img_url',
+                    recipeItems: [
+                        {
+                            id: 1,
+                            ingredient: {name: 'Mjöl'},
+                            amount: 3,
+                            unit: {name: "dl"}
+                        },
+                        {
+                            id: 2,
+                            ingredient: {name: 'Salt'},
+                            amount: 2,
+                            unit: {name: "krm"}
+                        }
+                    ]
+
+                }
+            ]
         };
     }
 
@@ -32,14 +69,27 @@ class App extends React.Component {
     }
 
     render() {
+        console.log(this.state.recipes);
+
+        const recipes = this.state.recipes.map((recipe) =>
+            <div>
+                <h2>{recipe.title}</h2>
+                <img src={recipe.imageUrl} alt={recipe.title}/>
+                <h3>Ingredienser</h3>
+                {
+                    recipe.recipeItems.map((item) =>
+                        <li>{item.ingredient.name} {item.amount} {item.unit.name}</li>)
+                }
+                <h3>Insruktioner</h3>
+                <p>{recipe.description}</p>
+            </div>
+        );
+
+
         return (
             <div>
                 <h1>Hello World!</h1>
-                <h2>{this.state.recipes[0].title}</h2>
-                <img src={this.state.recipes[0].imageUrl} alt={this.state.recipes[0].title}/>
-                <h3>Ingredienser</h3>
-                <h3>Insruktioner</h3>
-                <p>{this.state.recipes[0].description}</p>
+                {recipes}
             </div>
         )
     }
