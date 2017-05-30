e79f8968de8387099b7eea7c1be6dc2ca7375fa1
Christopher <christopher.olsson@hyperisland.se>
2017-05-30 09:37:04 +0200

    Changed Recipe to a functional component because of its conciseness

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,37 +1,35 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-export default class Recipe extends React.Component {
 
-    render() {
-        const recipe = this.props.recipe;
+export default function Recipe(props) {
 
-        if (this.props.detailsOnly) {
-            const shortDescription = recipe.description.substring(0, 100) + "...";
-            return (
-                <div>
-                    <h2>{recipe.title}</h2>
-                    <img src={recipe.imageUrl} alt={recipe.title}/>
-                    <p>{shortDescription}</p>
-                </div>
-            )
-        }
-
+    if (props.detailsOnly) {
+        const shortDescription = props.recipe.description.substring(0, 100) + "...";
         return (
             <div>
-                <h2>{recipe.title}</h2>
-                <img src={recipe.imageUrl} alt={recipe.title}/>
-                <h3>Ingredienser</h3>
-                {
-                    recipe.recipeItems.map((item) =>
-                        <li>{item.ingredient.name} {item.amount} {item.unit.name}</li>)
-                }
-                <h3>Instruktioner</h3>
-                <p>{recipe.description}</p>
+                <h2>{props.recipe.title}</h2>
+                <img src={props.recipe.imageUrl} alt={props.recipe.title}/>
+                <p>{shortDescription}</p>
             </div>
         )
     }
+
+    return (
+        <div>
+            <h2>{props.recipe.title}</h2>
+            <img src={props.recipe.imageUrl} alt={props.recipe.title}/>
+            <h3>Ingredienser</h3>
+            {
+                props.recipe.recipeItems.map((item) =>
+                    <li>{item.ingredient.name} {item.amount} {item.unit.name}</li>)
+            }
+            <h3>Instruktioner</h3>
+            <p>{props.recipe.description}</p>
+        </div>
+    )
 }
+
 
 Recipe.propTypes = {
     recipe: PropTypes.array,
