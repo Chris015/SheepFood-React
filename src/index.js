92db3c47bf4c0a8ee07f15c862a6b01b0c52713f
Christopher <christopher.olsson@hyperisland.se>
2017-05-30 07:45:04 +0200

    Refactored the rendering of recipes. Switched to Recipe Component

--- src/index.js
+++ src/index.js
@@ -7,12 +7,27 @@ import "./index.css";
 /*
  TODO:
  1. Förstasidan ska bara visa titel, bild och en liten del av 'description'
- 2. Loopa igenom listan med recept, och skapa recept-klasser
- 2.2 Skicka in information om receptet visa props t.ex <Recipe details = {recipeObjektet}/>
-
- Jag har inte tänkt igenom hur man begränsar vad man ska visa på framsidan jämfört när man klickar på ett recept.
- Vi får kanske skapa två typer av klasser. En som visar bara lite grann (framsidan) och en klass som visar allt (detaljvyn)
+    Begränsa details only med en if-sats if(this.props.detailsOnly)
  */
+
+class Recipe extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2>{this.props.recipe.title}</h2>
+                <img src={this.props.recipe.imageUrl} alt={this.props.recipe.title}/>
+                <h3>Ingredienser</h3>
+                {
+                    this.props.recipe.recipeItems.map((item) =>
+                        <li>{item.ingredient.name} {item.amount} {item.unit.name}</li>)
+                }
+                <h3>Instruktioner</h3>
+                <p>{this.props.recipe.description}</p>
+            </div>
+        )
+    }
+}
 
 
 class App extends React.Component {
@@ -31,32 +46,19 @@ class App extends React.Component {
             );
     }
 
-    generateRecipesJsx() {
-        return this.state.recipes.map((recipe) =>
-            <div>
-                <h2>{recipe.title}</h2>
-                <img src={recipe.imageUrl} alt={recipe.title}/>
-                <h3>Ingredienser</h3>
-                {
-                    recipe.recipeItems.map((item) =>
-                        <li>{item.ingredient.name} {item.amount} {item.unit.name}</li>)
-                }
-                <h3>Instruktioner</h3>
-                <p>{recipe.description}</p>
-            </div>
-        );
-
-    }
 
     render() {
         if (!this.state.recipes) {
             return <div className="loader"></div>
         } else {
-            const recipes = this.generateRecipesJsx();
             return (
                 <div>
                     <h1>Sheep Food</h1>
-                    {recipes}
+                    {
+                        this.state.recipes.map((recipe) =>
+                            <Recipe recipe={recipe} detailsOnly={true}/>
+                        )
+                    }
                 </div>
             )
         }
