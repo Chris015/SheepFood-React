a9b16198e4f26ebc808b491494a9e37fef51859a
Christopher <christopher.olsson@hyperisland.se>
2017-05-30 08:33:16 +0200

    repositioned the loader and added text

--- src/index.js
+++ src/index.js
@@ -7,12 +7,23 @@ import "./index.css";
 /*
  TODO:
  1. Förstasidan ska bara visa titel, bild och en liten del av 'description'
-    Begränsa details only med en if-sats if(this.props.detailsOnly)
+ Begränsa details only med en if-sats if(this.props.detailsOnly)
  */
 
 class Recipe extends React.Component {
 
     render() {
+
+        if (this.props.detailsOnly) {
+            return (
+                <div>
+                    <h2>{this.props.recipe.title}</h2>
+                    <img src={this.props.recipe.imageUrl} alt={this.props.recipe.title}/>
+                    <p>{this.props.recipe.description}</p>
+                </div>
+            )
+        }
+
         return (
             <div>
                 <h2>{this.props.recipe.title}</h2>
@@ -49,19 +60,25 @@ class App extends React.Component {
 
     render() {
         if (!this.state.recipes) {
-            return <div className="loader"></div>
-        } else {
             return (
-                <div>
-                    <h1>Sheep Food</h1>
-                    {
-                        this.state.recipes.map((recipe) =>
-                            <Recipe recipe={recipe} detailsOnly={true}/>
-                        )
-                    }
+                <div className="loader-content">
+                    <div className="loader-icon"/>
+                    <p>loading recipes...</p>
                 </div>
             )
         }
+
+        return (
+            <div>
+                <h1>Sheep Food</h1>
+                {
+                    this.state.recipes.map((recipe) =>
+                        <Recipe recipe={recipe} detailsOnly={true}/>
+                    )
+                }
+            </div>
+        )
+
     }
 
 
